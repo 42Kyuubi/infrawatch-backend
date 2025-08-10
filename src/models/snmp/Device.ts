c004e1160ebc5e72a94442cb5f91725e1f6e615b
Rafael Marcos <rafix995@gmail.com>
2025-08-10 19:39:17 +0100

    Device interface now has timestamp property

--- src/models/snmp/Device.ts
+++ src/models/snmp/Device.ts
@@ -5,10 +5,12 @@ import {System} from "./System";
  *
  * @interface Device
  * @property {string} ip - Device's IP address
- * @property {System} systemInfo = Relevant device's information
+ * @property {System} systemInfo - Relevant device's information
+ * @property {bigint} timestamp - the moment device data was collected in milliseconds
  * @readonly
  */
 export interface Device {
     readonly ip: string;
     readonly systemInfo: System;
+    readonly timestamp: bigint;
 }
